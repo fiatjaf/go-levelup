06274a2bc3469df73050c3fbcfaaa5bfe4e35480
fiatjaf <fiatjaf@gmail.com>
2017-03-27 23:17:39 +0000

    ensure is bloated, using check for the tests.

--- tests/tests.go
+++ tests/tests.go
@@ -3,28 +3,43 @@ package levelup_test
 import (
 	"testing"
 
-	"github.com/facebookgo/ensure"
 	"github.com/fiatjaf/levelup"
+	. "gopkg.in/check.v1"
 )
 
-func BasicTests(db levelup.DB, t *testing.T) {
-	/*** basic put, get, del ***/
+var db levelup.DB
+var err error
+
+func Test(sdb levelup.DB, t *testing.T) {
+	db = sdb
+	TestingT(t)
+}
+
+type BasicSuite struct{}
+
+var _ = Suite(&BasicSuite{})
+
+// the tests must be run in order. yes.
+// each suite depends on the the previous, that's why they have numbers in their names.
+
+func (s *BasicSuite) Test1PutGetDel(c *C) {
 	value, err := db.Get("key-x")
-	ensure.DeepEqual(t, err, levelup.NotFound)
-	ensure.DeepEqual(t, value, "")
+	c.Assert(err, DeepEquals, levelup.NotFound)
+	c.Assert(value, DeepEquals, "")
 
 	err = db.Put("key-x", "some value")
-	ensure.Nil(t, err)
+	c.Assert(err, IsNil)
 	value, _ = db.Get("key-x")
-	ensure.DeepEqual(t, value, "some value")
+	c.Assert(value, DeepEquals, "some value")
 
 	err = db.Del("key-x")
-	ensure.Nil(t, err)
+	c.Assert(err, IsNil)
 	value, err = db.Get("key-x")
-	ensure.DeepEqual(t, err, levelup.NotFound)
-	ensure.DeepEqual(t, value, "")
+	c.Assert(err, DeepEquals, levelup.NotFound)
+	c.Assert(value, DeepEquals, "")
+}
 
-	/*** batch put ***/
+func (s *BasicSuite) Test2BatchPut(c *C) {
 	somevalues := map[string]string{
 		"letter:a": "a",
 		"letter:b": "b",
@@ -38,9 +53,27 @@ func BasicTests(db levelup.DB, t *testing.T) {
 		batch = append(batch, levelup.Put(k, v))
 	}
 	err = db.Batch(batch)
-	ensure.Nil(t, err)
+	c.Assert(err, IsNil)
 
-	/*** reading ranges ***/
+	iter := db.ReadRange(nil)
+	retrieved := []string{}
+	for ; iter.Valid(); iter.Next() {
+		c.Assert(iter.Error(), IsNil)
+		retrieved = append(retrieved, iter.Key(), iter.Value())
+	}
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{
+		"letter:a", "a",
+		"letter:b", "b",
+		"letter:c", "c",
+		"number:1", "1",
+		"number:2", "2",
+		"number:3", "3",
+	})
+	iter.Release()
+}
+
+func (s *BasicSuite) Test3ReadRange(c *C) {
 	// start-end
 	iter := db.ReadRange(&levelup.RangeOpts{
 		Start: "letter:b",
@@ -48,10 +81,11 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved := []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{"letter:b", "b", "letter:c", "c"})
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{"letter:b", "b", "letter:c", "c"})
 	iter.Release()
 
 	// *-end
@@ -60,10 +94,11 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved = []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{"letter:a", "a", "letter:b", "b"})
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{"letter:a", "a", "letter:b", "b"})
 	iter.Release()
 
 	// start-* limit
@@ -73,10 +108,11 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved = []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{"letter:c", "c", "number:1", "1"})
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{"letter:c", "c", "number:1", "1"})
 	iter.Release()
 
 	// reverse
@@ -85,10 +121,11 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved = []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{
 		"number:3", "3", "number:2", "2", "number:1", "1",
 		"letter:c", "c", "letter:b", "b", "letter:a", "a",
 	})
@@ -102,10 +139,11 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved = []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{"number:1", "1", "letter:c", "c"})
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{"number:1", "1", "letter:c", "c"})
 	iter.Release()
 
 	// reverse *-end limit
@@ -116,9 +154,43 @@ func BasicTests(db levelup.DB, t *testing.T) {
 	})
 	retrieved = []string{}
 	for ; iter.Valid(); iter.Next() {
-		ensure.Nil(t, iter.Error())
+		c.Assert(iter.Error(), IsNil)
 		retrieved = append(retrieved, iter.Key(), iter.Value())
 	}
-	ensure.DeepEqual(t, retrieved, []string{"number:2", "2", "number:1", "1", "letter:c", "c"})
+	c.Assert(iter.Error(), IsNil)
+	c.Assert(retrieved, DeepEquals, []string{"number:2", "2", "number:1", "1", "letter:c", "c"})
 	iter.Release()
+}
+
+func (s *BasicSuite) Test4MoreBatches(c *C) {
+	batch := []levelup.Operation{
+		levelup.Del("number:2"),
+		levelup.Del("number:1"),
+		levelup.Put("number:3", "33"),
+		levelup.Del("number:4"),
+		levelup.Del("letter:a"),
+		levelup.Del("number:3"),
+		levelup.Del("letter:b"),
+		levelup.Del("letter:c"),
+		levelup.Put("number:3", "333"),
+		levelup.Del("letter:d"),
+		levelup.Put("letter:d", "dd"),
+		levelup.Del("letter:e"),
+	}
+	err = db.Batch(batch)
+	c.Assert(err, IsNil)
+
+	value, err := db.Get("number:1")
+	c.Assert(err, DeepEquals, levelup.NotFound)
+	c.Assert(value, DeepEquals, "")
+
+	value, err = db.Get("letter:e")
+	c.Assert(err, DeepEquals, levelup.NotFound)
+	c.Assert(value, DeepEquals, "")
+
+	value, _ = db.Get("number:3")
+	c.Assert(value, DeepEquals, "333")
+
+	value, _ = db.Get("letter:d")
+	c.Assert(value, DeepEquals, "dd")
 }
